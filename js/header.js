d3df4dd53bd740338181d833b276ccd9e9734db3
Gleb Gare.g <84378158+iamnotapr@users.noreply.github.com>
2022-01-28 16:24:56 +0700

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,19 @@
+let header_main = "";
+header_main += `
+<header>
+        <div class="div-header">
+            <div class="logo-item">Alpha</div>
+            <div class="link-alpha-8">
+                <a href="#alpha-model" class="pointer">Alpha-8</a>
+            </div>
+            <div class="space-station">
+                <a href="#launch-pad" class="pointer">Малина</a>
+            </div>
+        </div>
+    </header>
+`;
+const header = header_main;
+function headerF() {
+    document.getElementById('header-mine').innerHTML= header;
+}
+headerF();
