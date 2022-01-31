d1c7cf18fdd024ce98c8fb49c856eae040b5c365
Gleb Gare.g <84378158+iamnotapr@users.noreply.github.com>
2022-01-31 07:08:22 +0700

    Add files via upload

--- en/headerM.js
+++ en/headerM.js
@@ -1,23 +1,23 @@
-let header_main = "";
-header_main += `
+let headerEn = '';
+headerEn += `
 <header>
         <div class="div-header">
             <div class="logo-item">Alpha</div>
             <div class="link-alpha-8">
-                <a href="alpha-infen.html" class="pointer">Alpha-8</a>
+                <a href="en/alpha-infen.html" class="pointer">Alpha-8</a>
             </div>
             <div class="space-station">
-                <a href="#launch-pad" class="pointer">Малина</a>
+                <a href="#launch-pad" class="pointer">Malina</a>
             </div>
         </div>
         <div class="div-translate">
-        <a href="https://alphamodel.ga/en/alphasiteen.html">
+        <a href="https://alphamodel.ga/alphasite.html">
             <i class="bi bi-translate"></i>
         </a>
     </div>
     </header>
 `;
-const header = header_main;
+const header = headerEn;
 function headerF() {
     document.getElementById('header-mine').innerHTML= header;
 }
